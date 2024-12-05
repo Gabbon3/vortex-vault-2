6f0b64d83a6b2163f5f5988269481ad7c3bed2f6
Gabriele <51421142+Gabbon3@users.noreply.github.com>
2024-12-05 09:30:24 +0100

    fix dialect name

--- config/db.js
+++ config/db.js
@@ -13,7 +13,7 @@ export const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        dialect: 'postgree',
+        dialect: 'postgres',
         dialectOptions: {
             ssl: {
                 require: true,
