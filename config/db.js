6809132f19a354d0b8bd4e1ed84c3a85a4cbc554
Gabriele <g2004gabbo@icloud.com>
2025-01-06 18:18:37 +0100

    ready to deploy

--- config/db.js
+++ config/db.js
@@ -11,15 +11,15 @@ export const sequelize = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        dialect: 'mysql',
-        // dialect: 'postgres',
-        // dialectOptions: {
-        //     ssl: {
-        //         require: true,
-        //         rejectUnauthorized: false,
-        //         ca: fs.readFileSync(ssl_cert_path).toString(),
-        //     }
-        // },
+        // dialect: 'mysql',
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+                ca: fs.readFileSync(ssl_cert_path).toString(),
+            }
+        },
         logging: false,
         pool: {
             max: 5,           // Numero massimo di connessioni attive
