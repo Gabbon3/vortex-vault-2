e77ef5d5475fc0c6a24b69f7a963bb726c56a040
Gabriele <g2004gabbo@icloud.com>
2025-01-06 21:10:55 +0100

    fix

--- middlewares/passkey.middleware.js
+++ middlewares/passkey.middleware.js
@@ -41,7 +41,7 @@ export const verify_passkey = async_handler(async (req, res, next) => {
                 origin,
                 factor: "either",
                 publicKey: passkey.public_key,
-                prevCounter: BigInt(passkey.sign_count),
+                prevCounter: passkey.sign_count,
                 userHandle: credential.userHandle
             }
         );
